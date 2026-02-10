bfbd35b238140002cb13c5bbd1d9431165901b22
shyume <yu70536@hotmail.com>
2026-02-10 16:26:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkPassword() {
     var input = document.getElementById("passwordInput").value;
     
     // 設定密碼
-    var correctPassword = "123456";
+    var correctPassword = "cf38890765660682025170425310f845014389146162397775f042516462740e"; //adh
 
     if (input === correctPassword) {
         // 密碼正確：隱藏登入框，顯示內容
